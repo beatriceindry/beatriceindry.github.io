f07ed5c3d043568e724141642b8f22512fb8f202
beatriceindry <beatriceindry_19@kharisma.ac.id>
2021-01-26 02:21:20 -1000

    js

--- latihan6/js/init.js
+++ latihan6/js/init.js
@@ -5,14 +5,8 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, options);
-  });
 
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.collapsible').collapsible();
+    var elems = document.querySelector('.collapsible.expandable');
+    var instance = M.Collapsible.init(elems, {
+      accordion: false
   });
-       
